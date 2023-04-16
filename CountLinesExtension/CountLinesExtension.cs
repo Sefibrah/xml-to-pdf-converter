30da83eef0a5a954b8fd2452f19baebe549bbbef
Sefibrah <seferibrahim2@gmail.com>
2023-04-16 18:48:55 +0300

    The actual code itself

---
+++ CountLinesExtension/CountLinesExtension.cs
@@ -0,0 +1,72 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using SharpShell.Attributes;
+using SharpShell.SharpContextMenu;
+
+namespace CountLinesExtension
+{
+    /// <summary>
+    /// The CountLinesExtensions is an example shell context menu extension,
+    /// implemented with SharpShell. It adds the command 'Count Lines' to text
+    /// files.
+    /// </summary>
+    [ComVisible(true)]
+    [COMServerAssociation(AssociationType.ClassOfExtension, ".txt")]
+    public class CountLinesExtension : SharpContextMenu
+    {
+        /// <summary>
+        /// Determines whether this instance can a shell context show menu, given the specified selected file list.
+        /// </summary>
+        /// <returns>
+        ///   <c>true</c> if this instance should show a shell context menu for the specified file list; otherwise, <c>false</c>.
+        /// </returns>
+        protected override bool CanShowMenu()
+        {
+            //  We always show the menu.
+            return true;
+        }
+
+        /// <summary>
+        /// Creates the context menu. This can be a single menu item or a tree of them.
+        /// </summary>
+        /// <returns>
+        /// The context menu for the shell context menu.
+        /// </returns>
+        protected override ContextMenuStrip CreateMenu()
+        {
+            //  Create the menu strip.
+            var menu = new ContextMenuStrip();
+
+            //  Create a 'count lines' item.
+            var itemCountLines = new ToolStripMenuItem
+            {
+                Text = "Konvertuj XML u PDF..."
+                // Image = Properties.Resources.convert_tiny
+            };
+
+            //  When we click, we'll count the lines.
+            itemCountLines.Click += (sender, args) => ConvertXMLtoPDF();
+
+            //  Add the item to the context menu.
+            menu.Items.Add(itemCountLines);
+
+            //  Return the menu.
+            return menu;
+        }
+
+        /// <summary>
+        /// Converts XML to PDF
+        /// </summary>
+        private void ConvertXMLtoPDF()
+        {
+            foreach (var filePath in SelectedItemPaths)
+            {
+                var fileNameNoExt = Path.GetFileName(filePath).Split('.')[0];
+                var pdfContent = PdfCreator.FindPdfContent(filePath, "env:DocumentPdf");
+                PdfCreator.OpenPdf(fileNameNoExt, pdfContent);
+            }
+        }
+    }
+}
---
+++ CountLinesExtension/PdfCreator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Diagnostics;
+using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Pdf.Content.Objects;
+using PdfSharp.Drawing;
+
+namespace CountLinesExtension
+{
+    public class PdfCreator
+    {
+        public static string FindPdfContent(string filePath, string tagName)
+        {
+            // Load the XML file.
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(filePath);
+
+            // Find the tag with the specified name.
+            XmlNodeList nodes = xmlDoc.GetElementsByTagName(tagName);
+            return nodes[0].InnerText;
+        }
+
+        public static void OpenPdf(string fileNameNoExt, string pdfContent)
+        {
+            byte[] pdfBytes = Convert.FromBase64String(pdfContent);
+            using (var stream = new MemoryStream(pdfBytes))
+            {
+                var document = PdfReader.Open(stream);
+                string desktopFolder = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                string filePath = Path.Combine(desktopFolder, fileNameNoExt + ".pdf");
+                document.Save(filePath);
+                System.Diagnostics.Process.Start(filePath);
+            }
+        }
+    }
+}
